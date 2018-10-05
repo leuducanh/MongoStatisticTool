df1b4f209682d2c39556562d0d81c4530bd902bd
Leu Duc Anh <anh.leu@ntq-solution.com.vn>
2018-10-05 11:15:46 +0700

    done

--- src/main/java/vn/com/ntqsolution/MongoDbStatisticApplication.java
+++ src/main/java/vn/com/ntqsolution/MongoDbStatisticApplication.java
@@ -14,7 +14,7 @@ public class MongoDbStatisticApplication {
     public static void main(String[] args) {
         FileIOHelper.FOLDER_NAME = "statistic_api";
         Constant.STF_COLLECTION = "stf";
-        Constant.TIME_ZONE = "GMT";
+        Constant.TIME_ZONE = "GMT+0";
         MongoConfig.host = "localhost";
         MongoConfig.port = new Integer(27017);
         MongoConfig.connectionsPerHost = new Integer(1500);
